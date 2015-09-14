86554b7198dd1ff0837cfbaa2f6dc2ff7a456ec7
nchile <nilesh_chile@rediffmail.com>
2015-09-14 22:49:11 +0530

    test commit

---
+++ MyShoppingBasket/MyShoppingBasket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShoppingBasket.MvcApplication" Language="C#" %>
---
+++ MyShoppingBasket/MyShoppingBasket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShoppingBasket.Startup))]
+namespace MyShoppingBasket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
